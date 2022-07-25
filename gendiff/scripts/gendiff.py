0804157f04b484bcdb1a88fa12e62494ecf70ec2
seeu359 <a.ch3re359@yandex.ru>
2022-07-25 16:17:33 +0300

    Separation parser-data and point of entry.

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python
 import argparse
-from gendiff.gendiff import generate_diff
+from gendiff import generate_diff
+
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+first_file = parser.add_argument('first_file')
+second_file = parser.add_argument('second_file')
+parser.add_argument('-f', '--format', help='set format of output')
+args = parser.parse_args()
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    a = parser.add_argument('first_file')
-    b = parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
-    args = parser.parse_args()
-    return args
+    diff = generate_diff(args.first_file, args.second_file)
+    return diff
 
 
 if __name__ == '__main__':
