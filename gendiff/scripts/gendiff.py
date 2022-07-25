a8e1001fb2b2bfafbb5234a0eff50a297974b6ca
seeu359 <a.ch3re359@yandex.ru>
2022-07-25 21:22:10 +0300

    Fix flake8 errors

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,7 +2,9 @@
 import argparse
 from gendiff import generate_diff
 
-parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser = argparse.ArgumentParser(
+    description='Compares two configuration files'
+    ' and shows a difference.')
 first_file = parser.add_argument('first_file')
 second_file = parser.add_argument('second_file')
 parser.add_argument('-f', '--format', help='set format of output')
