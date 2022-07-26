765572d2fc68c1ec2ca18114dda7733b5efb5f14
seeu359 <a.ch3re359@yandex.ru>
2022-07-26 15:47:55 +0300

    Change path

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,7 +1,7 @@
-from gendiff.gendiff import generate_diff
+from gendiff import generate_diff
 
 
 def test_generate_diff():
-    result = generate_diff('/Users/a1234/python-project-lvl2/tests/fixtures/test_file1.json',
-                           '/Users/a1234/python-project-lvl2/tests/fixtures/test_file2.json')
-    assert result == open('/Users/a1234/python-project-lvl2/tests/fixtures/result_true.txt').read()
+    result = generate_diff('tests/fixtures/test_file1.json',
+                           'tests/fixtures/test_file2.json')
+    assert result == open('tests/fixtures/result_true.txt').read()
