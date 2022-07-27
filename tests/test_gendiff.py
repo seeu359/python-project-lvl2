55e825e7da3dc9da95fd37cef533c3005b2a6b47
seeu359 <a.ch3re359@yandex.ru>
2022-07-27 17:25:46 +0300

    Add new test for yaml files

--- tests/test_gendiff.py
+++
@@ -1,7 +0,0 @@
-from gendiff import generate_diff
-
-
-def test_generate_diff():
-    result = generate_diff('tests/fixtures/test_file1.json',
-                           'tests/fixtures/test_file2.json')
-    assert result == open('tests/fixtures/result_true.txt').read()
