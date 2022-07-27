2df4d7e9cb9049264eaa460944c461053e17d60e
seeu359 <a.ch3re359@yandex.ru>
2022-07-27 17:29:32 +0300

    Refactor for pep8

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,7 +3,7 @@ from gendiff import generate_diff
 
 def test_generate_diff():
     result_json = generate_diff('tests/fixtures/test_file1.json',
-                                        'tests/fixtures/test_file2.json')
+                                'tests/fixtures/test_file2.json')
     result_yaml = generate_diff('tests/fixtures/test_file1.yml',
                                 'tests/fixtures/test_file2.yaml')
     assert result_json == open('tests/fixtures/result_json_true.txt').read()
