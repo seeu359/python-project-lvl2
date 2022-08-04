e0385d63e09e293bf1983c63c5a6169cbbf77aec
seeu359 <a.ch3re359@yandex.ru>
2022-08-04 14:53:01 +0300

    Parser moved into parser.py module

---
+++ gendiff/parser.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def parsers_data():
+    parser = argparse.ArgumentParser(description='Compares two configuration'
+                                                 'files and shows '
+                                                 'a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        help='set format of output',
+                        default='stylish')
+    return parser.parse_args()
