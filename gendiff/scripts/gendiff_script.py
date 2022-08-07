ed0e41c67e622e6792d3f8b351a2950190132c87
seeu359 <a.ch3re359@yandex.ru>
2022-08-07 19:58:37 +0300

    Add new import

--- gendiff/scripts/gendiff_script.py
+++ gendiff/scripts/gendiff_script.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
-from gendiff.formatters.stylish import stylish
 from gendiff.generate_diff import generate_diff
 from gendiff.parser import parsers_data
+from gendiff.formatters.stylish import stylish
 from gendiff.formatters.plain import plain
 
 
 def main():
     args = parsers_data()
-    diff = (generate_diff(args.first_file, args.second_file))
+    if args.format == 'stylish':
+        return generate_diff(args.first_file, args.second_file, stylish)
     if args.format == 'plain':
-        return plain(diff)
-    else:
-        return stylish(diff)
+        return generate_diff(args.first_file, args.second_file, plain)
 
 
 if __name__ == '__main__':
