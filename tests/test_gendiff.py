aa361778d9c5fc8e9733b02832e3e4777d8b2683
seeu359 <a.ch3re359@yandex.ru>
2022-08-07 20:00:04 +0300

    Fix some tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,30 +2,40 @@ from gendiff.generate_diff import generate_diff
 from gendiff.formatters.stylish import stylish
 from gendiff.formatters.plain import plain
 
-RESULT_SIMPLE_FORMAT = open('tests/fixtures/result_simple_true.txt').read()
-RESULT_STYLISH_FORMAT = open('tests/fixtures/result_stylish_true.txt').read()
-RESULT_PLAIN_FORMAT = open('tests/fixtures/result_plain_true.txt').read()
+
+RESULT_SIMPLE_STYLISH = open('tests/fixtures/result_stylish_simple.txt').read()
+RESULT_SIMPLE_PLAIN = open('tests/fixtures/result_plain_simple').read()
+RESULT_STYLISH_FORMAT = open('tests/fixtures/result_stylish_nested.txt').read()
+RESULT_PLAIN_FORMAT = open('tests/fixtures/result_plain_nested.txt').read()
+
+gendiff_formatting_simple1 = \
+    generate_diff('tests/fixtures/test_file_simple1.json',
+                  'tests/fixtures/test_file_simple2.json', stylish)
+
+gendiff_formatting_simple2 = \
+    generate_diff('tests/fixtures/test_file_simple1.yml',
+                  'tests/fixtures/test_file_simple2.yaml', plain)
+
+gendiff_formatting_nested1 = \
+    generate_diff('tests/fixtures/test_file_nested1.yaml',
+                  'tests/fixtures/test_file_nested2.json', stylish)
+
+gendiff_formatting2_nested2 = \
+    generate_diff('tests/fixtures/test_file_nested1.json',
+                  'tests/fixtures/test_file_nested2.yaml', plain)
+
+
 
 
 def test_generate_diff_simple():
-    result_json_simple = generate_diff('tests/fixtures/test_file_simple1.json',
-                                       'tests/fixtures/test_file_simple2.json')
-    result_yaml_simple = generate_diff('tests/fixtures/test_file_simple1.yml',
-                                       'tests/fixtures/test_file_simple2.yaml')
-    assert isinstance(result_json_simple['follow'], list) is True
-    assert isinstance(result_yaml_simple['follow'], list) is True
-    assert result_json_simple['proxy'][2] == 1
-    assert result_yaml_simple['verbose'][2] == True
-    assert stylish(result_json_simple) == RESULT_SIMPLE_FORMAT
-    assert stylish(result_yaml_simple) == RESULT_SIMPLE_FORMAT
+    result_stylish_simple = gendiff_formatting_simple1
+    result_plain_simple = gendiff_formatting_simple2
+    assert result_stylish_simple == RESULT_SIMPLE_STYLISH
+    assert result_plain_simple == RESULT_SIMPLE_PLAIN
 
 
 def test_generate_diff_complex():
-    result_json_nested = generate_diff('tests/fixtures/test_file_nested1.json',
-                                       'tests/fixtures/test_file_nested2.json')
-    result_yaml_nested = generate_diff('tests/fixtures/test_file_nested1.yaml',
-                                       'tests/fixtures/test_file_nested2.yaml')
-    assert stylish(result_json_nested) == RESULT_STYLISH_FORMAT
-    assert stylish(result_yaml_nested) == RESULT_STYLISH_FORMAT
-    assert plain(result_yaml_nested) == RESULT_PLAIN_FORMAT
-    assert plain(result_json_nested) == RESULT_PLAIN_FORMAT
+    result_stylish_nested = gendiff_formatting_nested1
+    result_plain_nested = gendiff_formatting2_nested2
+    assert result_stylish_nested == RESULT_STYLISH_FORMAT
+    assert result_plain_nested == RESULT_PLAIN_FORMAT
