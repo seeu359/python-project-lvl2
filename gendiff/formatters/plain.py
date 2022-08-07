814287a7ffcb34f0b52322dabb8279baec7eb4ea
seeu359 <a.ch3re359@yandex.ru>
2022-08-07 20:41:08 +0300

    Refactoring plain function. All tests done

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -7,7 +7,7 @@ def normalize_type(value):
     if type(value) is bool:
         return f"{str(value).lower()}"
     elif value is None:
-        return f"null"
+        return 'null'
     else:
         return f"'{str(value)}'"
 
@@ -19,28 +19,38 @@ def plain(notion):
             if isinstance(data.get(key)[0], dict):
                 if data.get(key)[1] == '=':
                     get_format(data.get(key)[0], result, path + f'{key}.')
-                elif data.get(key)[1] == '-':
-                    if key.startswith('REP-'):
-                        result.append(f"Property '{path}{key[4:]}' "
-                                      f"{UPDATED} [complex value] to ")
-                    else:
-                        result.append(f"Property '{path}{key}' {REMOVED}\n")
-                elif key.startswith('REP+'):
-                    result.append('[complex value]\n')
-                elif data.get(key)[1] == '+':
-                    result.append(f"Property '{path}{key}' "
-                                  f"{ADDED} [complex value]\n")
+                else:
+                    result.append(formatting_dict(data, key, path))
             if not isinstance(data.get(key)[0], dict):
-                if key.startswith('REP-'):
-                    result.append(f"Property '{path}{key[4:]}' {UPDATED} "
-                                  f"{normalize_type(data.get(key)[0])} to ")
-                elif key.startswith('REP+'):
-                    result.append(f"{normalize_type(data.get(key)[0])}\n")
-                elif data.get(key)[1] == '+':
-                    result.append(f"Property '{path}{key}' {ADDED} "
-                                  f"{normalize_type(data.get(key)[0])}\n")
-                elif data.get(key)[1] == '-':
-                    result.append(f"Property '{path}{key}' {REMOVED}\n")
-
+                result.append(formatting_not_dict(data, key, path))
         return ''.join(result).strip()
+
     return get_format(notion, [], '')
+
+
+def formatting_dict(data, key, path):
+    if data.get(key)[1] == '-':
+        if key.startswith('REP-'):
+            return (f"Property '{path}{key[4:]}' "
+                    f"{UPDATED} [complex value] to ")
+        else:
+            return f"Property '{path}{key}' {REMOVED}\n"
+    elif key.startswith('REP+'):
+        return '[complex value]\n'
+    elif data.get(key)[1] == '+':
+        return f"Property '{path}{key}' {ADDED} [complex value]\n"
+
+
+def formatting_not_dict(data, key, path):
+    if key.startswith('REP-'):
+        return f"Property '{path}{key[4:]}' {UPDATED}" \
+               f" {normalize_type(data.get(key)[0])} to "
+    elif key.startswith('REP+'):
+        return f"{normalize_type(data.get(key)[0])}\n"
+    elif data.get(key)[1] == '+':
+        return f"Property '{path}{key}' {ADDED}" \
+               f" {normalize_type(data.get(key)[0])}\n"
+    elif data.get(key)[1] == '-':
+        return f"Property '{path}{key}' {REMOVED}\n"
+    else:
+        return ''
