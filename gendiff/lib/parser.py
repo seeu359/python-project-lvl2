77478c8be47add5b31323118fbc86ff770dbfec2
seeu359 <a.ch3re359@yandex.ru>
2022-08-18 20:40:47 +0300

    Rename functions, delete secondary_functions.py module, add data_handling and node_handling modules. Add in dev-deps pytes and pytest-cov. Rename json_format module in json. Little-bit change main script.

--- gendiff/lib/parser.py
+++ gendiff/lib/parser.py
@@ -4,10 +4,13 @@ import argparse
 def create_args():
     parser = argparse.ArgumentParser(description='Compares two configuration'
                                                  ' files and shows '
-                                                 'a difference.')
+                                                 'a difference. ')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format',
-                        help='set format of output',
+                        help='set format of output. Available output formats:'
+                             '1. stylish (default formatter); '
+                             '2. plain (-f plain); '
+                             '3. json (-f json)',
                         default='stylish', choices=['stylish', 'plain', 'json'])
     return parser.parse_args()
